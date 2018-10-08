b58e94e82c76defb1b2217c6102e6ec21eb35f03
toykarb <43945234+toykarb@users.noreply.github.com>
2018-10-08 13:21:18 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = '9dFJuMSOHv3AmbZbYrigGyvo3ltdPRk1JREPUDumm/xzjUQ/MTOrvpYXqC4ROq8ElOt9V/j0iW35Trpr9qqlMdy/Vucjg/nh44gISDQHlgxK3hjqNFZPXdHssA4OS2gg1Z1B2qBWT+pzM0XGwKF8pQdB04t89/1O/w1cDnyilFU=';
 
 
 $url = 'https://api.line.me/v1/oauth/verify';
